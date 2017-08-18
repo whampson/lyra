e462172e1c27327575183af95c5fc6f24f39c821
Wes Hampson <WesleyHampson@gmail.com>
2017-08-17 22:52:32 -0700

    Set up stack; reorganize code

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,14 +1,34 @@
-[ORG 0x7c00]
+    bits        16
+    org         0x7c00  ; entry point from BIOS
 
+    %assign     STACK_BASE      0x9000
+    %assign     BOOTSECT_MAGIC  0xaa55
+
+.section text
 start:
-    mov bx, MSG_HELLO
-    call print
-    jmp $
+    ; store boot drive id
+    mov         [BOOT_DRIVE], dl
 
-%include "print.asm"
+    ; set up the stack
+    mov         bp, 0x9000
+    mov         sp, bp
 
-MSG_HELLO db "Hello, world!", 13, 10, 0
+    ; do some funky stuff
+    mov         bx, MSG_HELLO
+    call        print
+    jmp         $               ; halt
 
-; padding and magic number
-times 510 - ($ - $$) db 0
-dw 0xaa55
+    ;%include    "disk.asm"
+    %include    "print.asm"
+
+.section data
+MSG_HELLO:
+    db          "Hello, world!", 13, 10, 0
+
+BOOT_DRIVE:
+    ; placeholder
+    db          0
+
+PADDING_MAGIC:
+    times       510 - ($ - $$) db 0
+    dw          BOOTSECT_MAGIC
