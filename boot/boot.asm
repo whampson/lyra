5390b1c61e9f3450289ea29842672a6d0da0dde1
Wes Hampson <WesleyHampson@gmail.com>
2017-08-17 21:07:50 -0700

    Print 'Hello, world!'

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,8 +1,34 @@
-[ORG 0x7C00]
+[ORG 0x7c00]
 
 start:
+    mov bx, MSG_HELLO
+    call print
     jmp $
+
+;--
+; Prints a zero-terminated string to the screen
+; at the current cursor position
+;
+; bx    - pointer to string
+;--
+print:
+    pusha
+    mov ah, 0x0e        ; int 0x10 function (TTY output mode)
+
+_print_loop:
+    mov al, [bx]        ; copy current character to 'al'
+    cmp al, 0           ; check for end of string...
+    je _print_done      ; ...and jump to end if true
+    int 0x10            ; print the character!
+    add bx, 1           ; increment string pointer
+    jmp _print_loop
+
+_print_done:
+    popa
+    ret
+
+MSG_HELLO db "Hello, world!", 13, 10, 0
 
 ; padding and magic number
 times 510 - ($ - $$) db 0
-dw 0xAA55
+dw 0xaa55
