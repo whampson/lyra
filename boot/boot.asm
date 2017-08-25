fd1dc73a7a48005f3c90cebfd2d3ac0a0d694f9a
Wes Hampson <WesleyHampson@gmail.com>
2017-08-24 19:03:56 -0500

    Run kernel code

--- boot/boot.asm
+++ boot/boot.asm
@@ -64,20 +64,8 @@ kernel_start:
     mov         ebp, STACK_BASE_PM
     mov         esp, ebp
 
-clear_screen:
-    mov         eax, 0xb8000
-    mov         ecx, 25 * 80
-
-clear_screen_loop:
-    mov    byte [eax], 0x00        ; char
-    mov    byte [eax + 1], 0x07    ; attr
-    add         eax, 2
-    sub         ecx, 1
-    jz          halt
-    jmp         clear_screen_loop
-
-halt:
-    jmp         $
+    ; pass control onto the kernel
+    jmp         KERNEL_BASE
 
     %include    "disk.asm"
     %include    "gdt.asm"
