e0b3561ac3869ce3c58964c28b1f73d32c23103d
Wes Hampson <thehambone93@gmail.com>
2018-03-21 02:13:39 -0500

    Small formatting fix

--- debug.sh
+++ debug.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-BOOT_FILE=boot
-
-gdb $BOOT_FILE.elf                      \
+gdb boot.elf                            \
     -ex 'target remote 10.0.2.2:1234'   \
     -ex 'set architecture i8086'        \
     -ex 'break entry'                   \
