476c0f327d177165174c0eeb8af77fcfefc328fa
Wes Hampson <thehambone93@gmail.com>
2018-03-20 20:46:47 -0500

    Replace build.sh with Makefile

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-as --32 -o a20.o a20.S
-as --32 -o boot.o boot.S
-as --32 -o i8042.o i8042.S
-ld -T bootloader.ld -o boot.elf a20.o boot.o i8042.o
-objcopy -O binary boot.elf boot.bin
