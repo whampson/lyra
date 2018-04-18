f70308eed8a011ce5f4e424f93dfe52a3967e11d
Wes Hampson <WesleyHampson@gmail.com>
2018-04-18 02:17:47 -0500

    Add kernel linker script

--- kernel/build.sh
+++ kernel/build.sh
@@ -2,5 +2,5 @@
 
 rm -f *.o *.bin *.elf
 gcc -Wall -Werror -Wextra -Wpedantic -m32 -ffreestanding -c -o kernel.o entry.c
-ld -Ttext=0x400000 -ekmain -melf_i386 -o kernel.elf kernel.o
+ld -T kernel.ld -o kernel.elf kernel.o
 objcopy -O binary kernel.elf kernel.bin
