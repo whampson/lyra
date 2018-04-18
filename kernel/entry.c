48c4d279593f1bf194a50c6b589f0eb940ba1b79
Wes Hampson <WesleyHampson@gmail.com>
2018-04-18 17:22:28 -0500

    Print message in white text on blue backgroud

--- kernel/entry.c
+++ kernel/entry.c
@@ -1,3 +1,8 @@
+#define BG_BLACK        0x00
+#define BG_BLUE         0x10
+#define FG_LIGHT_GRAY   0x07
+#define FG_WHITE        0x0F
+
 char * const VIDMEM = (char *) 0xb8000;
 static int char_ptr = 0;
 
@@ -22,7 +27,8 @@ void clear(void)
 {
     int i;
     for (i = 0; i < 80 * 25; i++) {
-        VIDMEM[i * 2] = '\0';
+        VIDMEM[i * 2] = ' ';
+        VIDMEM[i * 2 + 1] = BG_BLUE | FG_WHITE;
     }
 }
 
