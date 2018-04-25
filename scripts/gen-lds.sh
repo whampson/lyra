8bb011724efdbb2769b49abad3d4dab8282a7c85
Wes Hampson <thehambone93@gmail.com>
2018-04-24 20:43:33 -0500

    Add license header; pass along cflags

--- scripts/gen-lds.sh
+++ scripts/gen-lds.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+# Copyright (C) 2018 Wes Hampson. All Rights Reserved.                         #
+#                                                                              #
+# This file is part of the Lyra operating system.                              #
+#                                                                              #
+# Lyra is free software: you can redistribute it and/or modify                 #
+# it under the terms of version 2 of the GNU General Public License            #
+# as published by the Free Software Foundation.                                #
+#                                                                              #
+# See LICENSE in the top-level directory for a copy of the license.            #
+# You may also visit <https://www.gnu.org/licenses/gpl-2.0.txt>.               #
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+#-------------------------------------------------------------------------------
+# File: gen-lds.sh
+# Desc: "Preprocessor" for GNU linker scripts.
+#-------------------------------------------------------------------------------
+
 if [ $# -lt 1 ]; then
     echo "$0: usage: input_ld output_ld cflags"
     exit 1
 fi
 
-gcc -E -x c $1 | grep -v '^#' > $2
+gcc $3 -E -x c $1 | grep -v '^#' > $2
