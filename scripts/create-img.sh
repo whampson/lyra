93e0f903756661469e68a40a44b0ae3ffc81f108
Wes Hampson <thehambone93@gmail.com>
2018-04-24 21:33:42 -0500

    Add create-img.sh

--- scripts/create-img.sh
+++ scripts/create-img.sh
@@ -14,13 +14,38 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
 #-------------------------------------------------------------------------------
-# File: gen-lds.sh
-# Desc: "Preprocessor" for GNU linker scripts.
+# File: create-img.sh
+# Desc: Creates floppy disk image for Lyra.
 #-------------------------------------------------------------------------------
 
-if [ $# -lt 2 ]; then
-    echo "$0: usage: input_ld output_ld cflags"
+SECTOR_SIZE=512
+MAX_SECTOR_COUNT=18
+KERNEL_NUM_SECTORS=502
+
+if [ $# -lt 3 ]; then
+    echo "$0: usage: boot_img kernel_img out_img"
     exit 1
 fi
 
-gcc $3 -E -x c $1 | grep -v '^#' > $2
+boot_img=$1
+kernel_img=$2
+out_img=$3
+
+# Compute number of sectors needed to hold kernel image
+kernel_size=$(wc -c $kernel_img)
+kernel_size=${kernel_size/%\ */}    # separate size and filename
+num_sectors=$(awk -v s=$SECTOR_SIZE -v k=$kernel_size \
+    'BEGIN { print k / s + (s - (k % s)) / s }')
+
+# Combine boot and kernel images
+cat $boot_img $kernel_img > $out_img
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+# Update KERNEL_NUM_SECTORS value in disk image
+printf "\x$(printf %02x $num_sectors)" |\
+    dd of=$out_img bs=1 seek=$KERNEL_NUM_SECTORS conv=notrunc status=none
+if [ $? -ne 0 ]; then
+    exit 1
+fi
