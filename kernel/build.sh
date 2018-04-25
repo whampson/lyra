0f9ffec71c3d12e5e6ae8504b14b0c36a43f873c
Wes Hampson <thehambone93@gmail.com>
2018-04-24 21:49:43 -0500

    Remove old build script

--- kernel/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -f *.o *.bin *.elf
-gcc -Wall -Werror -Wextra -Wpedantic -m32 -ffreestanding -c -o kernel.o entry.c
-ld -T kernel.ld -o kernel.elf kernel.o
-objcopy -O binary kernel.elf kernel.bin
