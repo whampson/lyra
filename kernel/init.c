2a8b7166627eaeb612f4a9205588d4da43d71c96
Wes Hampson <thehambone93@gmail.com>
2018-05-04 12:50:10 -0500

    Print some strings to the screen

---
+++ kernel/init.c
@@ -0,0 +1,27 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+ * Copyright (C) 2018 Wes Hampson. All Rights Reserved.                       *
+ *                                                                            *
+ * This file is part of the Lyra operating system.                            *
+ *                                                                            *
+ * Lyra is free software: you can redistribute it and/or modify               *
+ * it under the terms of version 2 of the GNU General Public License          *
+ * as published by the Free Software Foundation.                              *
+ *                                                                            *
+ * See LICENSE in the top-level directory for a copy of the license.          *
+ * You may also visit <https://www.gnu.org/licenses/gpl-2.0.txt>.             *
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+/*-----------------------------------------------------------------------------
+ * File: init.c
+ * Desc: Kernel entry point and initialization.
+ *----------------------------------------------------------------------------*/
+
+#include <kernel.h>
+
+void kmain(void)
+{
+    clear();
+    puts("Hello, i386 world!\nNewline test.");
+    puts("aaa\rq\n");
+    puts("p\n");
+}
