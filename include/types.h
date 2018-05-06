586a4af1802b03c3b71b863ed37d360c47a4a871
Wes Hampson <thehambone93@gmail.com>
2018-05-06 14:06:35 -0500

    Add (u)int64_t

--- include/types.h
+++ include/types.h
@@ -22,9 +22,11 @@
 /* i386 integer types */
 typedef char int8_t;
 typedef unsigned char uint8_t;
-typedef short int16_t;
-typedef unsigned short uint16_t;
-typedef long int32_t;
-typedef unsigned long uint32_t;
+typedef short int int16_t;
+typedef unsigned short int uint16_t;
+typedef long int int32_t;
+typedef unsigned long int uint32_t;
+typedef long long int int64_t;
+typedef unsigned long long int uint64_t;
 
 #endif /* __TYPES_H__ */
