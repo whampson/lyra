0f9e562e14f05d7cdcbe1b5e57ab0f9af6b539a8
Wes Hampson <WesleyHampson@gmail.com>
2018-05-28 16:45:12 -0700

    Add kprintf_test()

---
+++ kernel/kprint_tests.c
@@ -0,0 +1,30 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+ * Copyright (C) 2018 Wes Hampson. All Rights Reserved.                       *
+ *                                                                            *
+ * This file is part of the Lyra operating system.                            *
+ *                                                                            *
+ * Lyra is free software: you can redistribute it and/or modify               *
+ * it under the terms of version 2 of the GNU General Public License          *
+ * as published by the Free Software Foundation.                              *
+ *                                                                            *
+ * See LICENSE in the top-level directory for a copy of the license.          *
+ * You may also visit <https://www.gnu.org/licenses/gpl-2.0.txt>.             *
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+/*-----------------------------------------------------------------------------
+ *   File: kernel/kprintf_tests.c
+ * Author: Wes Hampson
+ *----------------------------------------------------------------------------*/
+
+#ifdef __DEBUG
+
+#include <lyra/kernel.h>
+
+void kprintf_test(void)
+{
+    kprintf("Hello, world!\n");
+}
+
+#else
+extern int __dummy;
+#endif /* __DEBUG */
