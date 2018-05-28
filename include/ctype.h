e81e74d1c41ad6aaa527de19e698d038293edc93
Wes Hampson <thehambone93@gmail.com>
2018-05-28 01:19:07 -0700

    Implement ctype.h

--- include/ctype.h
+++ include/ctype.h
@@ -41,17 +41,17 @@
 #ifndef __CTYPE_H__
 #define __CTYPE_H__
 
+int iscntrl(int ch);
+int isprint(int ch);
+int isspace(int ch);
+int isblank(int ch);
+int isgraph(int ch);
+int ispunct(int ch);
 int isalnum(int ch);
 int isalpha(int ch);
-int isblank(int ch);
-int iscntrl(int ch);
-int isdigit(int ch);
-int isgraph(int ch);
 int islower(int ch);
-int isprint(int ch);
-int ispunct(int ch);
-int isspace(int ch);
 int isupper(int ch);
+int isdigit(int ch);
 int isxdigit(int ch);
 int tolower(int ch);
 int toupper(int ch);
