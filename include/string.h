2a9c310a4e86cf29a74db05f0ef47ebc1eefa535
Wes Hampson <WesleyHampson@gmail.com>
2018-05-28 22:58:07 -0700

    Fix prototypes to match style

--- include/string.h
+++ include/string.h
@@ -32,9 +32,8 @@
 typedef uint32_t size_t;
 #endif
 
-char * strcpy(char * dest, const char * src);
-char * strncpy(char * dest, const char * src, size_t n);
+char * strcpy(char *dest, const char *src);
+char * strncpy(char *dest, const char *src, size_t n);
 size_t strlen(const char * str);
-size_t strnlen(const char * str, size_t n);
 
 #endif /* __STRING_H */
