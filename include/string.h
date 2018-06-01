dbee79cda178a737116889982f6c9dfbe3cdd8a4
Wes Hampson <WesleyHampson@gmail.com>
2018-05-31 22:19:13 -0700

    Add strrev prototype

--- include/string.h
+++ include/string.h
@@ -36,4 +36,7 @@ char * strcpy(char *dest, const char *src);
 char * strncpy(char *dest, const char *src, size_t n);
 size_t strlen(const char *str);
 
+/* Non-standard but useful. */
+char * strrev(char *str);
+
 #endif /* __STRING_H */
