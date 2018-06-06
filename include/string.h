c3fe5e6406d17e04a0ffb9c29bc660363ac0a053
Wes Hampson <thehambone93@gmail.com>
2018-06-05 20:08:14 -0700

    Add memset

--- include/string.h
+++ include/string.h
@@ -133,6 +133,40 @@ static inline char * strrev(char *str)
 
 static inline void * memset(void *dest, int c, size_t n)
 {
+    unsigned char ch;
+    ch = (unsigned char) c;
+
+    __asm__ volatile (
+        "                               \n\
+    .memset_top%=:                      \n\
+        testl   %%ecx, %%ecx            \n\
+        jz      .memset_done%=          \n\
+        testl   $3, %%edi               \n\
+        jz      .memset_aligned%=       \n\
+        movb    %%al, (%%edi)           \n\
+        addl    $1, %%edi               \n\
+        subl    $1, %%ecx               \n\
+    .memset_aligned%=:                  \n\
+        movw    %%ds, %%dx              \n\
+        movw    %%dx, %%es              \n\
+        movl    %%ecx, %%edx            \n\
+        shrl    $2, %%ecx               \n\
+        andl    $3, %%edx               \n\
+        cld                             \n\
+        rep     stosl                   \n\
+    .memset_bottom%=:                   \n\
+        testl   %%edx, %%edx            \n\
+        jz      .memse_done%=           \n\
+        movb    %%al, (%%edi)           \n\
+        addl    $1, %%edi               \n\
+        subl    $1, %%edx               \n\
+    .memset_done%=:                     \n\
+        "
+        :
+        : "D"(dest), "a"(ch << 24 | ch << 16 | ch << 8 | ch), "c"(n)
+        : "edx", "memory", "cc"
+    );
+
     return dest;
 }
 
@@ -144,16 +178,16 @@ static inline void * memmove(void *dest, const void *src, size_t n)
         movw    %%dx, %%es              \n\
         cld                             \n\
         cmpl    %%edi, %%esi            \n\
-        jae     memmove_start%=         \n\
+        jae     .memmove_start%=        \n\
         leal    -1(%%esi, %%ecx), %%esi \n\
         leal    -1(%%edi, %%ecx), %%edi \n\
         std                             \n\
-        memmove_start%=:                \n\
+    .memmove_start%=:                   \n\
         rep     movsb                   \n\
         "
         :
         : "D"(dest), "S"(src), "c"(n)
-        : "cc", "memory"
+        : "edx", "cc", "memory"
     );
 
     return dest;
