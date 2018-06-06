1a43c7341cfe372bd30cb3720dd39eb8cbf6f6cd
Wes Hampson <thehambone93@gmail.com>
2018-06-05 23:38:41 -0700

    Add console

---
+++ include/lyra/console.h
@@ -0,0 +1,36 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+ * Copyright (C) 2018 Wes Hampson. All Rights Reserved.                       *
+ *                                                                            *
+ * This file is part of the Lyra operating system.                            *
+ *                                                                            *
+ * Lyra is free software: you can redistribute it and/or modify               *
+ * it under the terms of version 2 of the GNU General Public License          *
+ * as published by the Free Software Foundation.                              *
+ *                                                                            *
+ * See LICENSE in the top-level directory for a copy of the license.          *
+ * You may also visit <https://www.gnu.org/licenses/gpl-2.0.txt>.             *
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+/*-----------------------------------------------------------------------------
+ *   File: include/lyra/kernel.h
+ * Author: Wes Hampson
+ *   Desc: Commonly-used and convenient function prototypes and macros.
+ *----------------------------------------------------------------------------*/
+
+#ifndef __LYRA_CONSOLE_H
+#define __LYRA_CONSOLE_H
+
+#define NUM_CONSOLES    8
+#define CON_COLS        80
+#define CON_ROWS        25
+#define CON_AREA        (CON_COLS * CON_ROWS)   /* total characters */
+#define CON_SIZE        (CON_AREA * 2)          /* total bytes */
+
+void console_init(void);
+
+int console_puts(const char *s);
+void console_putchar(char c);
+
+void set_console(int num);
+
+#endif /* __LYRA_CONSOLE_H */
