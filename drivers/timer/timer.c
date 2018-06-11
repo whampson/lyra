2b073ba7ef8657882d194bfc877918b4b7259eaf
Wes Hampson <thehambone93@gmail.com>
2018-06-10 17:36:21 -0700

    Implement timer_set_rate()

--- drivers/timer/timer.c
+++ drivers/timer/timer.c
@@ -17,17 +17,74 @@
  *   Desc: Programmable Interval Timer (PIT) driver.
  *----------------------------------------------------------------------------*/
 
+#include <stdint.h>
 #include <drivers/timer.h>
+#include <lyra/io.h>
+
+/* PIT clock rate */
+#define PIT_CLK         1193182
+
+/* I/O ports */
+#define PORT_PIT_CH0    0x40
+#define PORT_PIT_CH1    0x41
+#define PORT_PIT_CH2    0x42
+#define PORT_PIT_CMD    0x43
+
+/* Channel masks */
+#define CH0             0x00
+#define CH1             0x40
+#define CH2             0x80
+
+/* Access mode masks */
+#define LATCH           0x00
+#define LOBYTE          0x10
+#define HIBYTE          0x20
+#define LOHIBYTE        0x30
+
+/* Operating mode masks */
+#define SQUARE_WAVE     0x06
 
 int beep_ticks = 0;
 
-void timer_set_rate(int hz)
+void timer_set_rate(int ch, int hz)
 {
+    uint16_t timer_port;
+    uint16_t divisor;
+    uint8_t ch_mask;
+    uint8_t cfg;
 
+    switch (ch) {
+        case 0:
+            timer_port = PORT_PIT_CH0;
+            ch_mask = CH0;
+            break;
+        case 2:
+            timer_port = PORT_PIT_CH2;
+            ch_mask = CH2;
+            break;
+        default:
+            return;
+    }
+
+    if (hz > TIMER_MAX_FREQ) {
+        hz = TIMER_MAX_FREQ;
+    }
+    divisor = PIT_CLK / hz;
+
+    /* Configure channel */
+    cfg = ch_mask | LOHIBYTE | SQUARE_WAVE;
+    outb(cfg, PORT_PIT_CMD);
+
+    /* Set tick frequency */
+    outb((uint8_t) divisor, timer_port);
+    outb((uint8_t) (divisor >> 8), timer_port);
 }
 
 /**
  * Timer tick interrupt handler.
  */
-void timer_do_irq(void);
+void timer_do_irq(void)
+{
+
+}
 
